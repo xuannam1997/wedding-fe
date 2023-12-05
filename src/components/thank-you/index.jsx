2cb4512b1e460db9db6a1cac25ac7b5b556f89c8
Dinh Xuan Nam <dinhxuannam20121997@gmail.com>
2023-12-05 23:17:51 +0700

    fix

--- src/components/thank-you/index.jsx
+++ src/components/thank-you/index.jsx
@@ -5,19 +5,15 @@ const ThankYou = () => {
     <section className="hero scroll-mt-20 mt-20 bg-cover bg-center md:min-h-[500px] w-full relative">
       <div className="hero-overlay bg-white/10 z-20"></div>
       <Image
-        src="/images/bg-7.webp"
+        src="/images/thank_you.jpg"
         alt=""
         className="object-cover object-center pointer-events-none z-10"
         fill
         loading="lazy"
       />
       <div className="container m-auto flex flex-col relative z-50 items-center px-5 py-20">
-        <h3 className="text-5xl md:text-[100px] text-white font-black text-center drop-shadow">
-          Thank you!
-        </h3>
-        <p className="text-3xl md:text-[50px] text-white font-bold text-center drop-shadow">
-          See you at the wedding!
-        </p>
+        <h3 className="text-5xl md:text-[100px] text-white font-black text-center drop-shadow"></h3>
+        <p className="text-3xl md:text-[50px] text-white font-bold text-center drop-shadow"></p>
       </div>
     </section>
   );
